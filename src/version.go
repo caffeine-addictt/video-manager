88d81a4d8b9fb3751de4018acb3ebd31e88e1c18
AlexNg <contact@ngjx.org>
2024-04-04 23:37:57 +0800

    feat: Add version command

---
+++ src/version.go
@@ -0,0 +1,19 @@
+package src
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of video-manager",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.0.0")
+	},
+}
+
+func init() {
+	rootCommand.AddCommand(versionCommand)
+}
