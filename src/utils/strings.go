7780d7d93b461e338256a6f7146729d4125d510a
AlexNg <contact@ngjx.org>
2024-04-09 20:58:43 +0800

    feat: Add input prompt util

--- src/utils/strings.go
+++ src/utils/strings.go
@@ -1,7 +1,26 @@
 package utils
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Multiline(line ...string) string {
 	return strings.Join(line, "\n")
+}
+
+func InputPrompt(text string) string {
+	// Tailing space need not be added
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, text+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
 }
