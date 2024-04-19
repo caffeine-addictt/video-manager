ba2ddf8e26050a83d8b2cb9938e379fba1750e9d
AlexNg <contact@ngjx.org>
2024-04-19 18:02:55 +0800

    chore(version): Bump version to 0.0.7
    Sorry i forgot to bump it to 0.0.6 previously!!

--- src/version.go
+++ src/version.go
@@ -10,7 +10,7 @@ var versionCommand = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of video-manager",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.5")
+		fmt.Println("v0.0.7")
 	},
 }
 
